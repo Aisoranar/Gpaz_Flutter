089a92317714af477ee2ba03e66da72a6282eb7f
Aisoranar <aisoranaya20@gmail.com>
2024-03-16 21:48:19 -0500

    Se mejoro la precisión del gps

--- lib/tabs/second_tab.dart
+++ lib/tabs/second_tab.dart
@@ -21,7 +21,7 @@ class _SecondTabState extends State<SecondTab> {
   Location _location = Location();
   late StreamSubscription<LocationData> _locationSubscription;
   bool _locationUpdatesActive = false;
-  late Marker _user1Marker; // Agrega una variable para el marcador del usuario 1
+  late Marker _user1Marker; 
 
   @override
   void initState() {
@@ -38,7 +38,7 @@ class _SecondTabState extends State<SecondTab> {
       _user1Marker = Marker(
         markerId: MarkerId('user1Marker'),
         position: _user1Location,
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue), // Icono azul para el usuario 1
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue), 
         infoWindow: InfoWindow(title: 'Usuario 1'),
       );
     });
@@ -58,14 +58,10 @@ class _SecondTabState extends State<SecondTab> {
           if (latitud != null && longitud != null) {
             _user2Location = LatLng(latitud, longitud);
             _showPolyline = true;
-
-            // Mostrar notificación cuando se actualiza la ubicación en SecondTab
             notificationsManager.showNotification(
               'Conductor en Camino',
               'El conductor va en camino.',
             );
-
-            // Mostrar notificación global cuando se selecciona una opción
             notificationsManager.showGlobalNotification(
               'Notificación Global',
               'Opción seleccionada por el conductor.',
@@ -73,14 +69,10 @@ class _SecondTabState extends State<SecondTab> {
           } else {
             _user2Location = null;
             _showPolyline = false;
-
-            // Mostrar notificación cuando el bus finaliza la ruta en SecondTab
             notificationsManager.showNotification(
               'Conductor Finalizó Ruta',
               'El conductor ha finalizado la ruta.',
             );
-
-            // Mostrar notificación global cuando se selecciona otra opción
             notificationsManager.showGlobalNotification(
               'Notificación Global',
               'Otra opción seleccionada por el conductor.',
@@ -90,7 +82,6 @@ class _SecondTabState extends State<SecondTab> {
       }
     });
 
-    // Suscribirse a las actualizaciones de ubicación del usuario 1
     _locationSubscription = _location.onLocationChanged.listen((LocationData currentLocation) {
       setState(() {
         _myLocationData = currentLocation;
@@ -99,7 +90,7 @@ class _SecondTabState extends State<SecondTab> {
           _user1Marker = Marker(
             markerId: MarkerId('user1Marker'),
             position: _user1Location,
-            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue), // Icono azul para el usuario 1
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue), 
             infoWindow: InfoWindow(title: 'Usuario 1'),
           );
         }
@@ -137,12 +128,12 @@ class _SecondTabState extends State<SecondTab> {
           _controller.complete(controller);
         },
         markers: {
-          _user1Marker, // Usar el marcador actualizado del usuario 1
+          _user1Marker, 
           if (_user2Location != null)
             Marker(
               markerId: MarkerId('user2Marker'),
               position: _user2Location!,
-              icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen), // Icono verde para el usuario 2
+              icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen), 
               infoWindow: InfoWindow(title: 'Usuario 2'),
             ),
           if (_myLocationData != null)
@@ -156,9 +147,6 @@ class _SecondTabState extends State<SecondTab> {
             ),
         },
         myLocationEnabled: true,
-        onCameraMove: (CameraPosition position) {
-          // Puedes agregar lógica adicional al mover el mapa si es necesario
-        },
         gestureRecognizers: Set()
           ..add(Factory<ScaleGestureRecognizer>(() => ScaleGestureRecognizer()))
           ..add(Factory<TapGestureRecognizer>(() => TapGestureRecognizer()))
